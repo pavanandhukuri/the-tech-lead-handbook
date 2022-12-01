30f80fb3e88af7ba5826d3ab53713e8be0b8b897
Pavan Andhukuri <pavan.andhukuri@thoughtworks.com>
2022-12-01 18:33:45 +0530

    Pavan | Init chapter 2

---
+++ 02.developer-productivity/index.js
@@ -0,0 +1,13 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function sayHello() {
+    readline.question("> What's your name?\n", (name) => {
+        console.log("> Hello " + name + "!");
+        readline.close();
+    });
+}
+
+sayHello();
