2da860b2cf36e544e6114a3f6ccbc90b3f586cdc
Pavan Andhukuri <pavan.andhukuri@thoughtworks.com>
2023-01-22 15:18:16 +0530

    Pavan | add prettier

--- 02.developer-productivity/index.js
+++ 02.developer-productivity/index.js
@@ -1,13 +1,13 @@
 const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 function sayHello() {
-    readline.question("> What's your name?\n", (name) => {
-        console.log("> Hello " + name + "!");
-        readline.close();
-    });
+  readline.question("> What's your name?\n", (name) => {
+    console.log('> Hello ' + name + '!');
+    readline.close();
+  });
 }
 
 sayHello();
